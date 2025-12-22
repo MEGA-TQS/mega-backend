4169b3136b9e2b232f7076c7626581dd2f774f72
Bernardo Lazaro <bernardolazaro@ua.pt>
2025-12-22 20:00:52 +0000

    cors fix

--- MEGA-backend/src/main/java/com/sportsgear/rentalplatform/config/WebConfig.java
+++ MEGA-backend/src/main/java/com/sportsgear/rentalplatform/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
                 // Replace with your frontend URL (Vite usually uses 5173, Create-React-App uses 3000)
-                .allowedOrigins("http://localhost", "http://localhost:80", "http://192.168.160.41", "http://192.168.160.41:80", "http://deti-tqs-17.ua.pt", "http://deti-tqs-17.ua.pt:80") 
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
